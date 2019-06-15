08415fe1fa4c6b4b581cd985726fae55a5179d7f
Rebecca Tong <rtong@buffalo.edu>
2019-06-15 15:58:10 -0400

    created signup for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def home
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :name, :bio, :skintype)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,19 @@
 ActiveRecord::Schema.define(version: 2019_06_15_032757) do
 
   create_table "makeup_products", force: :cascade do |t|
+    t.string "name"
+    t.string "purpose"
+    t.boolean "waterproof"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.integer "makeup_products_id"
+    t.integer "user_id"
+    t.integer "rating"
+    t.string "title"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
