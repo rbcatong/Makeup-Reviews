03ae917fde00675c945af85c61b7eaf25c4f8762
Rebecca Tong <rtong@buffalo.edu>
2019-06-15 16:07:37 -0400

    created signin page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+     flash[:error] = "Sorry, we could not find the combination, try again or signup."
+      redirect_to '/users/new'
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Username has been used."
+      redirect_to new_user_path
+    end
   end
 
     private
