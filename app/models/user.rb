6ad1906ca4ff3a4740380d6f6736cb5137bc4d45
Rebecca Tong <rtong@buffalo.edu>
2019-06-18 17:13:08 -0400

    establishing connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  has_many :routines
+  has_many :reviews
+  has_many :makeup_products through, :reviews
   has_secure_password
 end
