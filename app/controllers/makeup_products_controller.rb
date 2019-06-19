2c5781239b7535531b6796e0bc0a5aa6a41509a8
Rebecca Tong <rtong@buffalo.edu>
2019-06-19 16:30:42 -0400

    added list of makeup productS

--- app/controllers/makeup_products_controller.rb
+++ app/controllers/makeup_products_controller.rb
@@ -4,6 +4,18 @@ class MakeupProductsController < ApplicationController
   end
 
   def create
-    @makeup_product = MakeupProduct.create
+    @makeup_product = MakeupProduct.create(makeup_product_params)
+    if @makeup_product.save
+      redirect_to makeup_products_path
+    end
+  end
+
+  def index
+    @makeup_products = MakeupProduct.all
+  end
+
+  private
+  def makeup_product_params
+    params.require(:makeup_product).permit(:name, :purpose, :waterproof)
   end
 end
--- app/models/makeup_product.rb
+++ app/models/makeup_product.rb
@@ -1,4 +1,4 @@
 class MakeupProduct < ApplicationRecord
   has_many :reviews
-  has_many :users through, :reviews
+  has_many :users, through: :reviews
 end
