5840e47a54f169599e15d634982ad049d4bd70e0
Rebecca Tong <rtong@buffalo.edu>
2019-06-21 17:13:15 -0400

    fixed associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :reviews
-  has_many :makeups, through: :reviews
+  has_many :reviewed_makeups, through: :reviews, source: :makeup
+  has_many :makeups
   has_secure_password
   validates :username, presence: true, uniqueness: true
 end
