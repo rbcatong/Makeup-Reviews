78ccf03075aefe4a40f692709eff757ab10a95ff
Rebecca Tong <rtong@buffalo.edu>
2019-07-01 16:11:06 -0400

    showing users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    binding.pry
+    @user = User.find_by(id: params[:user_id])
   end
 
     private
