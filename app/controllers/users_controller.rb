9cc675b5926b5da644afa03aa106a6bebb535804
Rebecca Tong <rtong@buffalo.edu>
2019-07-02 13:13:27 -0400

    able to see profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
+      binding.pry
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :makeups, through: :reviews
 
-  has_secure_password
+  has_secure_password 
   validates :username, presence: true, uniqueness: true
 
   def self.add_points
