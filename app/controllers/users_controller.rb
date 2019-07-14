28742dcc3f47dd3db934a7285c9004fdf8e2a00b
Rebecca Tong <rtong@buffalo.edu>
2019-07-14 14:41:39 -0400

    able to edit skintype for propfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,10 +33,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user.id
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    @user = User.update(bio: params[:user][:bio], skintype: params[:user][:skintype])
+    redirect_to user_path
   end
 
     private
